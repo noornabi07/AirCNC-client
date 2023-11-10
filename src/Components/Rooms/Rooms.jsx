677f6e74a82bf433177e82c770f014d9f216833d
noornabi07 <noornabiprogram07@gmail.com>
2023-11-10 10:16:09 +0600

    using search text keep and navigate change url

--- src/Components/Rooms/Rooms.jsx
+++ src/Components/Rooms/Rooms.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from 'react';
 import Container from '../Shared/Container';
 import Card from './Card';
 import Loader from '../Shared/Loader/Loader';
+import { useSearchParams } from 'react-router-dom';
 
 const Rooms = () => {
+    const [params, setParams] = useSearchParams();
+    const category = params.get('category')
     const [rooms, setRooms] = useState([]);
     const [loading, setLoading] = useState(false);
 
